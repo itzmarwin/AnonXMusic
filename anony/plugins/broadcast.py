454fbb85883a59cac174e9407fe4f0be3ee2ff7c
itzmarwin <itzm4rw1n@gmail.com>
2026-06-05 16:57:24 +0530

    Update broadcast.py
    Signed-off-by: itzmarwin <itzm4rw1n@gmail.com>

--- anony/plugins/broadcast.py
+++ anony/plugins/broadcast.py
@@ -2,7 +2,6 @@
 # Licensed under the MIT License.
 # This file is part of AnonXMusic
 
-
 import os
 import asyncio
 
@@ -10,8 +9,8 @@ from pyrogram import errors, filters, types
 
 from anony import app, db, lang
 
-
 broadcasting = asyncio.Lock()
+
 
 @app.on_message(filters.command(["broadcast"]) & app.sudoers)
 @lang.language()
@@ -24,47 +23,78 @@ async def _broadcast(_, message: types.Message):
 
     msg = message.reply_to_message
     copy = "-copy" in message.command
-    count, ucount = 0, 0
-    groups, users = set(), set()
     sent = await message.reply_text(message.lang["gcast_start"])
 
-    if "-nochat" not in message.command:
-        groups = set(await db.get_chats())
-    if "-user" in message.command:
-        users = set(await db.get_users())
-
-    chats = list(groups | users)
-    failed = None
-
     async with broadcasting:
-        for chat in chats:
+
+        count = 0
+        failed = None
+
+        if "-nochat" not in message.command:
+            groups = list(await db.get_chats())
+
+            for chat in groups:
+                try:
+                    (
+                        await msg.copy(chat, reply_markup=msg.reply_markup)
+                        if copy
+                        else await msg.forward(chat)
+                    )
+                    count += 1
+                    await asyncio.sleep(0.2)
+                except errors.FloodWait as fw:
+                    await asyncio.sleep(fw.value + 10)
+                except Exception as ex:
+                    if not failed:
+                        failed = open("errors.txt", "w")
+                    failed.write(f"{chat} - {ex}\n")
+                    continue
+
+        await sent.edit_text(
+            f"» 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍𝖾𝖽 𝖬𝖾𝗌𝗌𝖺𝗀𝖾𝗌 𝖳𝗈 {count}  𝖢𝗁𝖺𝗍𝗌"
+        )
+
+        if "-user" not in message.command:
+            if failed:
+                failed.close()
+                await message.reply_document(document="errors.txt")
+                try:
+                    os.remove("errors.txt")
+                except Exception:
+                    pass
+            return
+
+        ucount = 0
+        users = list(await db.get_users())
+
+        for user in users:
             try:
                 (
-                    await msg.copy(chat, reply_markup=msg.reply_markup)
+                    await msg.copy(user, reply_markup=msg.reply_markup)
                     if copy
-                    else await msg.forward(chat)
+                    else await msg.forward(user)
                 )
-                if chat in groups:
-                    count += 1
-                else:
-                    ucount += 1
+                ucount += 1
                 await asyncio.sleep(0.2)
             except errors.FloodWait as fw:
                 await asyncio.sleep(fw.value + 10)
             except Exception as ex:
                 if not failed:
-                    failed = open("errors.txt", "w")
-                failed.write(f"{chat} - {ex}\n")
+                    failed = open("errors.txt", "a") 
+                failed.write(f"{user} - {ex}\n")
                 continue
 
-    text = message.lang["gcast_end"].format(count, ucount)
+    final_text = f"» 𝖡𝗋𝗈𝖺𝖽𝖼𝖺𝗌𝗍𝖾𝖽 𝖬𝖾𝗌𝗌𝖺𝗀𝖾𝗌 𝖳𝗈 {ucount}  𝖴𝗌𝖾𝗋𝗌"
+
     if failed:
         failed.close()
         await message.reply_document(
             document="errors.txt",
-            caption=text,
+            caption=final_text,
         )
-        try: os.remove("errors.txt")
-        except Exception: pass
-
-    await sent.edit_text(text)
+        try:
+            os.remove("errors.txt")
+        except Exception:
+            pass
+    else:
+        await message.reply_text(final_text)
