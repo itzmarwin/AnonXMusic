b2341745f92a1163ceb5b53f913a08d874e8bc8a
itzmarwin <itzm4rw1n@gmail.com>
2026-09-05 15:45:39 +0530

    Update faileddb.py
    Signed-off-by: itzmarwin <itzm4rw1n@gmail.com>

--- anony/plugins/faileddb.py
+++ anony/plugins/faileddb.py
@@ -1,8 +1,3 @@
-# Copyright (c) 2025 AnonymousX1025
-# Licensed under the MIT License.
-# This file is part of AnonXMusic
-
-
 import os
 
 from pyrogram import filters, types
@@ -14,13 +9,13 @@ from anony import app, lang
 @lang.language()
 async def faileddb_func(_, m: types.Message):
     if m.from_user.id not in app.sudoers:
-        return await m.reply_text(m.lang["faileddb_not_allowed"])
+        return await m.reply_text("Ye command sirf owner/sudo use kar sakta hai.")
 
     file_path = "failed_songs.txt"
     if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
-        return await m.reply_text(m.lang["faileddb_empty"])
+        return await m.reply_text("Abhi tak koi failed song nahi mila.")
 
     await m.reply_document(
         document=file_path,
-        caption=m.lang["faileddb_caption"],
+        caption="Failed songs ki list.",
     )
